6fab67693f92802e638b2a3e33dfc301830f0b7d
joaopjk <joaopjk48@gmail.com>
2021-07-03 20:51:38 -0300

    Relação entre obj de querys

--- schema-query/index.js
+++ schema-query/index.js
@@ -7,21 +7,35 @@ const usuarios = [{
     email: "joao@gmail.com",
     idade: 26,
     salario_real: 6000.56,
-    vip: true
+    vip: true,
+    perfil_id: 1
 }, {
     id: 2,
     nome: "Franci Iara Sousa",
     email: "francini@gmail.com",
     idade: 300,
     salario_real: 6000.56,
-    vip: true
+    vip: true,
+    perfil_id: 3
 }, {
     id: 3,
     nome: "João Cícero Vicente Sousa",
     email: "joao@gmail.com",
     idade: 26,
     salario_real: 6000.56,
-    vip: true
+    vip: true,
+    perfil_id: 2
+}]
+
+const perfis = [{
+    id: 1,
+    nome: 'Junior'
+}, {
+    id: 2,
+    nome: "Pleno"
+}, {
+    id: 3,
+    nome: 'Senior'
 }]
 
 const typeDefs = gql` 
@@ -34,6 +48,7 @@ const typeDefs = gql`
         idade: Int
         salario: Float
         vip: Boolean
+        perfil: Perfil
     }
 
     type Produto {
@@ -42,6 +57,11 @@ const typeDefs = gql`
         preco: String!
         desconto: Float
         precoComDesconto: Float
+    }
+
+    type Perfil{
+        id: ID!
+        nome: String!
     }
 
     # Portas de entrada da API. O sinal ! obriga o retorno(não pode ser nullo)
@@ -53,12 +73,18 @@ const typeDefs = gql`
         usuario(id:ID): Usuario
         produtoEmDestaque: Produto
         numerosMegaSena: [Int!]!
+        perfil(id: Int): Perfil
+        perfis: [Perfil]
     }
 `
 const resolvers = {
     Usuario: {
         salario(usuario) {
             return usuario.salario_real;
+        },
+        perfil(usuario) {
+            const perfil = perfis.filter(p => p.id === usuario.perfil_id);
+            return perfil ? perfil[0] : null;
         }
     },
     Produto: {
@@ -103,6 +129,13 @@ const resolvers = {
         usuario(_, { id }) {
             const selecionados = usuarios.filter(x => x.id == id);
             return selecionados ? selecionados[0] : null;
+        },
+        perfis() {
+            return perfis;
+        },
+        perfil(_, { id }) {
+            perfil = perfis.filter(p => p.id == id)
+            return perfil ? perfil[0] : null;
         }
     }
 }
