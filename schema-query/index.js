74692472aefc21718ce48592a4c8b2b1ec9c0fb1
joaopjk <joaopjk48@gmail.com>
2021-07-03 18:20:55 -0300

    Criando tipo de retorno

--- schema-query/index.js
+++ schema-query/index.js
@@ -4,10 +4,20 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql` 
     scalar Date
 
+    type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     # Portas de entrada da API. O sinal ! obriga o retorno(não pode ser nullo)
     type Query {
         ola: String
         horaCerta: Date
+        usuarioLogado: Usuario
     }
 `
 const resolvers = {
@@ -17,6 +27,16 @@ const resolvers = {
         },
         horaCerta() {
             return new Date;
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: "João Cícero Vicente Sousa",
+                email: "joao@gmail.com",
+                idade: 26,
+                salario: 6000.00,
+                vip: true
+            }
         }
     }
 }
