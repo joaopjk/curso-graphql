50b6fe83396696b81ede22df783634e43716d85c
joaopjk <joaopjk48@gmail.com>
2021-07-05 19:55:41 -0300

    Utilizando Input

--- schema-query/resolvers/mutation.js
+++ schema-query/resolvers/mutation.js
@@ -1,17 +1,17 @@
 const { usuarios, proximoId } = require('../data/db');
 
 module.exports = {
-    novoUsuario(_, { nome, email, idade }) {
-        const emailExistente = usuarios.some(u => u.email = email);
+    novoUsuario(_, { dados}) {
+        const emailExistente = usuarios.some(u => u.email = dados.email);
 
         if (emailExistente)
             throw new Error("Email já cadastrado !");
 
         const novo = {
             id: proximoId(),
-            nome,
-            email,
-            idade,
+            nome:dados.nome,
+            email:dados.email,
+            idade:dados.idade,
             perfil_id: 1,
             status: 'ATIVO'
         }
@@ -29,17 +29,14 @@ module.exports = {
         return excluidos ? excluidos[0] : null;
     },
 
-    alterarUsuario(_, { id, nome, email, idade }) {
+    alterarUsuario(_, { dados }) {
         const i = usuarios.findIndex(u => u.id === id);
 
         if (i < 0) return null;
 
         const usuario = {
             ...usuarios[i],
-            id,
-            nome,
-            email,
-            idade
+            ...dados
         }
 
         usuarios.splice(i, 1, usuario);
