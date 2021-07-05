e77ddeaca18c052f9da8ec7c086dc0b6ac46e2df
joaopjk <joaopjk48@gmail.com>
2021-07-05 19:40:18 -0300

    Mutation para excluir um Usuario

--- schema-query/resolvers/mutation.js
+++ schema-query/resolvers/mutation.js
@@ -2,20 +2,30 @@ const { usuarios, proximoId } = require('../data/db');
 
 module.exports = {
     novoUsuario(_, { nome, email, idade }) {
-        const emailExistente = usuarios.some( u => u.email = email);
+        const emailExistente = usuarios.some(u => u.email = email);
 
-        if(emailExistente)
+        if (emailExistente)
             throw new Error("Email já cadastrado !");
 
-        const novo ={
-            id:proximoId(),
+        const novo = {
+            id: proximoId(),
             nome,
             email,
             idade,
-            perfil_id:1,
-            status:'ATIVO'
+            perfil_id: 1,
+            status: 'ATIVO'
         }
         usuarios.push(novo);
         return novo;
+    },
+
+    excluirUsuario(_, { id }) {
+        const i = usuarios.findIndex(u => u.id === id);
+
+        if (i < 0) return null;
+
+        const excluidos = usuarios.splice(i, 1);
+
+        return excluidos ? excluidos[0] : null;
     }
 }
