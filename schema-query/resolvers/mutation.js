1dc004cf93434b75adf3e0578b6c67188476ea44
joaopjk <joaopjk48@gmail.com>
2021-07-05 20:13:31 -0300

    Utilizando Input

--- schema-query/resolvers/mutation.js
+++ schema-query/resolvers/mutation.js
@@ -1,7 +1,19 @@
 const { usuarios, proximoId } = require('../data/db');
 
+function indiceUsuario(filtro) {
+    if (!filtro) return -1;
+
+    const { id, email } = filtro;
+    if (id)
+        return usuarios.findIndex(u => u.id === id);
+    if (email)
+        return usuarios.findIndex(u => u.email === email);
+    
+    return -1;
+}
+
 module.exports = {
-    novoUsuario(_, { dados}) {
+    novoUsuario(_, { dados }) {
         const emailExistente = usuarios.some(u => u.email = dados.email);
 
         if (emailExistente)
@@ -9,9 +21,9 @@ module.exports = {
 
         const novo = {
             id: proximoId(),
-            nome:dados.nome,
-            email:dados.email,
-            idade:dados.idade,
+            nome: dados.nome,
+            email: dados.email,
+            idade: dados.idade,
             perfil_id: 1,
             status: 'ATIVO'
         }
@@ -19,8 +31,8 @@ module.exports = {
         return novo;
     },
 
-    excluirUsuario(_, { id }) {
-        const i = usuarios.findIndex(u => u.id === id);
+    excluirUsuario(_, { filtro}) {
+        const i = indiceUsuario(filtro);
 
         if (i < 0) return null;
 
